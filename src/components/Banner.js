501a7f08486419fea174b44e6e27f7f334ac536e
bronwyncarr <bronwynacarr@gmail.com>
2021-01-05 15:13:19 +1100

    Initial commit

---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import './Banner.css'
+
+function Banner() {
+return(
+  <div class="container heading-info">
+  <h1 class="container-text">BRONWYN CARR</h1>
+  <h2>- Developer - Pilot - </h2>
+</div>
+)
+}
+export default Banner
