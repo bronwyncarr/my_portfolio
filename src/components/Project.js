8b1ada5486aaf06fc3ffc372fc9adc10a2e53ae7
bronwyncarr <bronwynacarr@gmail.com>
2021-01-05 20:20:06 +1100

    flip card for project

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,21 @@
-import './Project.css'
+import "./Project.css";
 
-function Project({ name, details, pic}) {
+function Project({ name, details, pic, stack, link}) {
   return (
-    <div className="project">
-      <img src={pic} alt=""></img>
-      <h4>{name}</h4>
-      <p>{details}</p>
+    <div className="flip-card">
+      <div class="flip-card-inner">
+        <div class="flip-card-front">
+          <img src={pic} alt=""></img>
+          <h4>{name}</h4>
+          <p>{stack}</p>
+        </div>
+        <div class="flip-card-back">
+          <p>{details}</p>
+          <a href={link} rel="noreferrer" target="_blank"><i class="fab fa-github"></i></a>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Project
+export default Project;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,6 +10,8 @@ function Projects() {
           name="Waste Me Not"
           pic="https://images.unsplash.com/photo-1488459716781-31db52582fe9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
           details="Reduce food waste everyday by using up ingredients before they reach their use-by date."
+          stack="Ruby, CSS"
+          link="https://github.com/bronwyncarr/waste_me_not"
         />
         <Project />
         <Project />
