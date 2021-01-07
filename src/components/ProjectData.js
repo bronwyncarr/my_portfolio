dfc5d29da123baeb002cc9e30a7ecb9d5604c5a7
bronwyncarr <bronwynacarr@gmail.com>
2021-01-08 09:41:19 +1100

    changes icon color and project data desc

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -9,8 +9,7 @@ const ProjectData = [
   },
   {
     name: "Amendment Tracker",
-    pic:
-    "../../img/amend.png",
+    pic: "../../img/amend.png",
     details:
       "The client for this project was an international airline. They wanted to track a significant amount of operational manual amendments that were required. Speed to production was their first priority.",
     stack: "Ruby on Rails, Boostrap, PostgreSQL",
@@ -18,8 +17,7 @@ const ProjectData = [
   },
   {
     name: "50x Mini Projects",
-    pic:
-    "../../img/movie.png",
+    pic: "../../img/movie.png",
     details:
       "Mini projects to demostrate specific CSS and JavaScript skills. Based on the '50 Projects in 50 Days', course each project has been altered or extended to demonstrate technique.",
     stack: "CSS, JavaScript",
@@ -27,8 +25,7 @@ const ProjectData = [
   },
   {
     name: "Waste Me Not",
-    pic:
-    "../../img/terminal.png",
+    pic: "../../img/terminal.png",
     details:
       "In Australia, 1 in 5 bags of food shopping bought is thrown out. Reduce food waste and save money by searching for recipes to use ingredients before they reach their use by date.",
     stack: "Ruby",
@@ -36,8 +33,7 @@ const ProjectData = [
   },
   {
     name: "Tic Tac Toe",
-    pic:
-    "../../img/tictactoe.png",
+    pic: "../../img/tictactoe.png",
     details:
       "Simple take on the classic game to demonstrate React functionailty.",
     stack: "React, CSS",
@@ -45,13 +41,12 @@ const ProjectData = [
   },
   {
     name: "Tax Tidy",
-    pic:
-    "../../img/tax.png",
+    pic: "../../img/tax.png",
     details:
-      "Inspired by being an ATO Tax help volunteer, this app is designed to help simplify parts of tax for those that find it overwhelming. Keep track of your deductions in one place.",
+      "Inspired by being an ATO Tax help volunteer, this app is designed to help simplify parts of tax compliance for those that find it overwhelming. Keep track of your deductions and receipts in one place.",
     stack: "Ruby on Rails, Bootstrap, PostgreSQL, AWS",
     link: "https://github.com/bronwyncarr/tax_tidy",
   },
 ];
 
-export default ProjectData
+export default ProjectData;
